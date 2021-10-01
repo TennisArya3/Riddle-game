42082d66c092bebb6a3a526f7f0b315df3b41134
TennisArya3 <83346420+TennisArya3@users.noreply.github.com>
2021-10-01 14:49:46 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "Mercury";
+const accessCode2 = "Candle";
+const accessCode3 = "Footsteps";
+
+function clues() {
+    
+    fill("black")
+    textSize(15)
+    text("I am god and planet! And used to measure temp.", 200,210)
+    fill("lightblue")
+    text("Hint: Closest planet to the Sun!!", 200,230)
+
+    fill("black")
+    textSize(15)
+    text("I’m tall when I’m young, and I’m short when I’m old.  ", 640,300)
+    fill("lightblue")
+    text("Hint: Was eariler used for light !!", 700,320)
+
+    fill("black")
+    textSize(15)
+    text("The more you take, the more you leave behind. ", 1100,250)
+    fill("lightblue")
+    text("Hint: Starts with the letter F !!", 1100,270)
+
+}
